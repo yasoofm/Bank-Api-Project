e714e44422a931ebe51c515e8af56f310b8cf1a1
Fawaz Alzamel <fawazzamel1@gmail.com>
2024-01-31 10:12:24 +0300

    Added signin

--- app/src/main/java/com/joincoded/bankapi/utils/Constants.kt
+++ app/src/main/java/com/joincoded/bankapi/utils/Constants.kt
@@ -4,6 +4,7 @@ class Constants {
     companion object {
         const val baseUrl = "https://coded-bank-api.eapi.joincoded.com/"
         const val signupEndpoint = "signup"
+        const val signinEndpoint = "signin"
         const val depositEndpoint = "deposit"
         const val authorization = "Authorization"
     }
