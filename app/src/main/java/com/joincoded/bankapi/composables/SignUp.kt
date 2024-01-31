4ba3fe8a3fb22c8420e7c2209f7a6d3ec5aca021
mubarak <aloraifan505@hotmail.com>
2024-01-31 10:11:35 +0300

    done with signup card

--- app/src/main/java/com/joincoded/bankapi/composables/SignUp.kt
+++ app/src/main/java/com/joincoded/bankapi/composables/SignUp.kt
@@ -5,7 +5,6 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
@@ -16,7 +15,6 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import com.joincoded.bankapi.viewmodel.BankViewModel
 
