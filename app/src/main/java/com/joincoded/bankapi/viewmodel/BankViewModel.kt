4c2d71d582c0ad6df7fd1df04fe51daa45b380cf
yousef mandani <ytmandani@outlook.com>
2024-01-31 11:18:58 +0300

    fixed small errors

--- app/src/main/java/com/joincoded/bankapi/viewmodel/BankViewModel.kt
+++ app/src/main/java/com/joincoded/bankapi/viewmodel/BankViewModel.kt
@@ -21,7 +21,7 @@ class BankViewModel : ViewModel() {
     fun signup(username: String, password: String, image: String = "") {
         viewModelScope.launch {
             try {
-                val response = apiService.signup(User(username, password, image, null, null))
+                val response = apiService.signup(User(username, password, null, image, null))
                 token = response.body()
             } catch (e: Exception) {
                 println("Error $e")
@@ -33,7 +33,7 @@ class BankViewModel : ViewModel() {
     fun signin(username: String, password: String) {
         viewModelScope.launch {
             try {
-                val response = apiService.signin(User(username, password, null, null))
+                val response = apiService.signin(User(username, password, null, null, null))
                 token = response.body()
             } catch (e: Exception) {
                 println("Error $e")
